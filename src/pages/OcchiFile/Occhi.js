684268f541f92ebaf798cf6f766577b885343838
FredericGuerin <fguerin555@gmail.com>
2025-06-08 21:09:23 +0200

    ModifpouranciensNavigateurs

--- src/pages/OcchiFile/Occhi.js
+++ src/pages/OcchiFile/Occhi.js
@@ -67,7 +67,7 @@ const Occhi = () => {
               <iframe
                 width="560"
                 height="315"
-                src="https://www.youtube.com/embed/4AdbmWKmA0k"
+                src="https://www.youtube.com/embed/jd80lJqmoOA"
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
